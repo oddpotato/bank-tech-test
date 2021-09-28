7c9b672759ba08a0cce7eaa4ee88c85fad9ce2c1
Fiona Apps <wikipanyd@gmail.com>
2021-09-28 10:47:40 +0100

    slight edit

--- lib/account.rb
+++ lib/account.rb
@@ -33,13 +33,13 @@ class Account
   end
 
   def format_date
-    # This utilizes the 'string formation time' method - see full documentation below
+    # This utilizes the 'string format time' method - see full documentation below
     # https://ruby-doc.org/stdlib-2.4.2/libdoc/date/rdoc/Date.html#method-i-strftime
     @stripped_date = Date.today.strftime('%d-%m-%Y')
   end
 
   def format_money(amount)
-    # This utilizes the sprintf float formatting method - see full documentation below
+    # This utilizes the sprintf formatting method - see full documentation below
     # https://ruby-doc.org/core-2.1.2/Kernel.html#method-i-sprintf
     @fixed_amount = format('%05.2f', amount)
   end
